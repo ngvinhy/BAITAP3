1d9bdc66c7146e620c60d9cc61a2a0475f058d03
ngvinhy <ngvinhy@users.noreply.github.com>
2023-02-28 14:44:17 +0700

    ngvinhy

--- BAI1.py
+++ BAI1.py
@@ -15,31 +15,37 @@ while True:
 4. Sắp xếp
 5. Xóa list
 6. Thoát chương trình""")
-    chucnang = int(input("Chọn chức năng muốn sử dụng: "))
-    if chucnang == 1:
-        a = int(input("Nhập giá trị muốn thêm: "))
-        lst.append(a)
-        print(f"List hiện tại: {lst}")
-    elif chucnang == 2:
-        k = int(input("Nhập giá trị k cần kiểm tra: "))
-        dem = lst.count(k)
-        print(f"Giá trị {k} xuất hiện {dem} lần trong list")
-    elif chucnang == 3:
-        tong = 0
-        for a in lst:
-            if ktrasonguyento(int(a)):
-                tong += a
-        print(f"Tổng các số nguyên tố trong list là: {tong}")
-    elif chucnang == 4:
-        if lst:
-            lst.sort()
-            print(f"List được sắp xếp: {lst}")
+    try:
+        chucnang = int(input("Chọn chức năng muốn sử dụng: "))
+        if chucnang == 1:
+            try:
+                a = int(input("Nhập giá trị muốn thêm: "))
+                lst.append(a)
+                print(f"List hiện tại: {lst}")
+            except ValueError:
+                print("List bao gồm các số nguyên")
+        elif chucnang == 2:
+            k = int(input("Nhập giá trị k cần kiểm tra: "))
+            dem = lst.count(k)
+            print(f"Giá trị {k} xuất hiện {dem} lần trong list")
+        elif chucnang == 3:
+            tong = 0
+            for a in lst:
+                if ktrasonguyento(int(a)):
+                    tong += a
+            print(f"Tổng các số nguyên tố trong list là: {tong}")
+        elif chucnang == 4:
+            if lst:
+                lst.sort()
+                print(f"List được sắp xếp: {lst}")
+            else:
+                print("List rỗng")
+        elif chucnang == 5:
+            lst.clear()
+            print("List đã được xóa")
+        elif chucnang == 6:
+            break
         else:
-            print("List rỗng")
-    elif chucnang == 5:
-        lst.clear()
-        print("List đã được xóa")
-    elif chucnang == 6:
-        break
-    else:
+            print("Lựa chọn không hợp lệ, vui lòng chọn lại")
+    except ValueError:
         print("Lựa chọn không hợp lệ, vui lòng chọn lại")
